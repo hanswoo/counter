24e564ccebb366669f8b475f03366eec94f22b24
D7702_10 <d7702_10@D7702-10ui-iMac.local>
2018-03-21 12:22:51 +0900

    return

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
-
+var count = 0
+var color = 0
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lab: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func bt(_ sender: Any) {
+        count = count + 1;
+        lab.text = "\(count)"
+    }
+    
+    @IBAction func bts(_ sender: Any) {
+        count = 0
+        lab.text = "\(count)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
